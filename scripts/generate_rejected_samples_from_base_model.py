2c052e6594bd2453d17d51c11e3bcbce2ec7e8f2
Deepak Karkala <dkarkala01@gmail.com>
2025-02-20 12:41:51 +0530

    Updating script to generate rejected samples

--- scripts/generate_rejected_samples_from_base_model.py
+++ scripts/generate_rejected_samples_from_base_model.py
@@ -21,12 +21,12 @@ file_path = os.path.abspath(sys.argv[1])
 with open(file_path, "r") as file:
     args = yaml.safe_load(file)  # Use safe_load to avoid security risks
 
-df = pd.read_parquet(args.dataset.data_input_path)
+df = pd.read_parquet(args["dataset"]["data_input_path"])
 
 for i in range(len(df)):
     message_system_user = df.iloc[i]["prompt"]
     try:
-        response: ChatResponse = chat(model=args.model.model_ollama, messages=message_system_user)
+        response: ChatResponse = chat(model=args["model"]["model_ollama"], messages=message_system_user)
         message_rejected = {}
         message_rejected["content"] = response['message']['content']
         message_rejected["role"] = "assistant"
@@ -35,4 +35,4 @@ for i in range(len(df)):
         logger.info(f"LLM reply failed for sample: {i}")
         df.loc[i]["rejected"] = None
 
-df.to_parquet(args.dataset.data_output_path)
+df.to_parquet(args["dataset"]["data_output_path"])
